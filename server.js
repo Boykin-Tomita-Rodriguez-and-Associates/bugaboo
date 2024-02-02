33b5cb7bbad7d147236fc1acf8e8395535d97a09
Courtlyn Boykin <courtlynb@google.com>
2024-02-02 16:35:45 -0500

    added GET routes for project

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./server/app');
 const { db } = require('./server/db')
 const port = 3000
 
-app.listen(PORT, () => {
+app.listen(port, () => {
   db.sync({ force: false });
-  console.log(`App is listening at http://localhost:${PORT}`);
+  console.log(`App is listening at http://localhost:${port}`);
 });
