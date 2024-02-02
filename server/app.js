a7e274ab65c7f8f3c973a82c3db9e8a3bc982a65
Courtlyn Boykin <courtlynb@google.com>
2024-02-02 16:35:45 -0500

    added GET routes for project

--- server/app.js
+++ server/app.js
@@ -2,8 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const projectRouter = require('./routes/project');
 
 //add routers here eventually 
 
 app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
+//routers
+app.use('/projects', projectRouter);
+
+// 404 handler
+app.use((req, res) => {
+    res.status(404).send({error: '404 - Not Found', message: 'No route found for the requested URL'});
+  });
+  module.exports = app;
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,3 +2,6 @@ const { Router } = require('express');
 const { User, Project, Bug } = require('../models/index');
 const express = require('express'); 
 
+const userRouter = Router()
+
+//
