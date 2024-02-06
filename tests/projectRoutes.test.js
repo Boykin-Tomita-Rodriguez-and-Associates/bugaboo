57fa2d428cddf19771cd9c52aecd2aab95cf7f13
shamito <shamito@google.com>
2024-02-06 17:17:56 -0500

    update for projects and tests

--- tests/projectRoutes.test.js
+++ tests/projectRoutes.test.js
@@ -64,4 +64,33 @@ describe("Project testing", () => {
       expect(projects.length).toEqual(projectsLength - 1);
     });
   });
+
+  describe("Update a project", () => {
+    it("Successfully updates a project", async () => {
+        const response = await request(app)
+        .put("/projects/1")
+        .send({name: "Updated project name"}); 
+        
+        const updatedProject = await Project.findByPk(1);
+ 
+        expect(response.statusCode).toBe(200);
+        expect(response.body[0].name).toBe(updatedProject.name)
+    });
+    
+    it("Successfully adds a bug", async()=>{
+        const bug = {error: "meep meep"};
+        const response = await request(app)
+        .put("/projects/1")
+        .send({bug: bug});
+
+        const updatedProject = await(Project).findByPk(1, {
+            include: {
+                model: Bug
+            }
+        });
+
+        expect(response.statusCode).toBe(200); 
+        expect(updatedProject.bugs[1].error).toBe(bug.error)
+    })
+  })
 });
